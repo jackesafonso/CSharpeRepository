1136b6a020ba517e43cf39971bc6bd0ff3dc926c
afonsogoncalves@msn.com <afonsogoncalves@msn.com>
2016-06-20 16:33:27 +0100

    create

---
+++ listOfBooks/listOfBooks/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listOfBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] listOfFruits = { "orange", "mango", "banana", "apple" };
+
+            List<string> myFruits = new List<string>() { "orange", "mango", "banana", "apple" };
+
+            Console.WriteLine("Here you have the list of fruits");
+            Console.WriteLine("to exit press x");
+            char response = Convert.ToChar(Console.ReadLine().ToUpper());
+
+            do
+            {
+                switch(response)
+                {
+                    case 'A':
+
+                }
+
+
+            }
+
+            //you can sort List<string>!
+            list.Sort();
+
+            list.Add("lemon");
+            list.Remove("mango");
+
+
+            //list.Remove("4");//remove specieifed item.
+            // list.RemoveAt(2);//remove item from index.
+            // list.RemoveRange(1, 2);//remove a range of items.
+
+            Console.WriteLine(); 
+            Console.ReadLine(); 
+
+        }
+
+        
+    }
+}
---
+++ switchStatement/switchStatement/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switchStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ronaldo, Suarez, Neymar, Ronaldo are running for best football player in the world who do you think will win?");
+            string input;
+
+            do
+            {
+                Console.WriteLine("Who will win? choose between: neymar, suarez, messi, ronaldo");
+                input = Console.ReadLine();
+
+
+
+            switch (input)
+                {
+                    case "neymar":
+                    case "messi":
+                        Console.WriteLine("Good choice he probably wins as he is one of the best player in the world!");
+
+                        break;
+
+                    case "ronaldo":
+
+                        Console.WriteLine("No good enough");
+
+                        break;
+
+                    default:
+                        Console.WriteLine("I'm sorry, I don't understand that!");
+
+                        break;
+                }
+            } while (input != "i dont know");
+    
+            Console.WriteLine("Press a key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
